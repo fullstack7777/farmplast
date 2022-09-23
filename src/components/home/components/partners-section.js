7080d4d873097935e5985e119b9ccb74d1638d18
bega <akiyev9@gmail.com>
2022-09-24 04:55:54 +0500

    added partner sections

--- src/components/home/components/partners-section.js
+++ src/components/home/components/partners-section.js
@@ -12,34 +12,34 @@ function PartnersSection() {
                         <div className="brands_slider_container">
                             <div className="owl-carousel owl-theme brands_slider">
                                 <div className="owl-item">
-                                    <a href={'https://www.kazanorgsintez.ru/'}  target="_blank" className="brands_item d-flex flex-column justify-content-center">
+                                    <a href={'https://www.kazanorgsintez.ru/'}  target="_blank" className="brands_item d-flex flex-column justify-content-center" rel="noreferrer">
                                         <img height={'100'}
-                                            src="/images/partners/partner-1.png"
+                                            src={"/images/partners/partner-1.png"}
                                             alt=""/>
                                     </a>
                                 </div>
                                 <div className="owl-item">
                                     <div className="brands_item d-flex flex-column justify-content-center">
                                         <img height={'100'}
-                                             src="/images/partners/partner-2.png"
+                                             src={"/images/partners/partner-2.png"}
                                              alt=""/></div>
                                 </div>
                                 <div className="owl-item">
                                     <div className="brands_item d-flex flex-column justify-content-center">
                                         <img height={'100'}
-                                             src="/images/partners/partner-3.png"
+                                             src={"/images/partners/partner-3.png"}
                                              alt=""/></div>
                                 </div>
                                 <div className="owl-item">
                                     <div className="brands_item d-flex flex-column justify-content-center">
                                         <img height={'100'}
-                                             src="/images/partners/partner-4.png"
+                                             src={"/images/partners/partner-4.png"}
                                              alt=""/></div>
                                 </div>
                                 <div className="owl-item">
                                     <div className="brands_item d-flex flex-column justify-content-center">
                                         <img height={'100'}
-                                             src="/images/partners/partner-5.png"
+                                             src={"/images/partners/partner-5.png"}
                                              alt=""/></div>
                                 </div>
                             </div>
