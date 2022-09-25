8f6cfc2f534e476b0c28fbdb79802ecb39bd04ca
bega <akiyev9@gmail.com>
2022-09-25 23:44:24 +0500

    added products page

--- src/components/warehouse/warehouse_page.js
+++ src/components/warehouse/warehouse_page.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
 
 export class WarehousePage extends React.Component {
     render() {
         return (
             <div>
+                <Container>
 
+                </Container>
             </div>
         );
     }
