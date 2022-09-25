14111d605b4c5825505e1e5a59c2b9abeeef739f
bega <akiyev9@gmail.com>
2022-09-26 00:12:39 +0500

    added products page

--- src/components/home/components/slider-section.js
+++ src/components/home/components/slider-section.js
@@ -4,36 +4,24 @@ import Carousel from 'react-bootstrap/Carousel';
 function MainSlider() {
     const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex, e) => {
+    const handleSelect = (selectedIndex, _) => {
         setIndex(selectedIndex);
     };
 
     return (
         <Carousel activeIndex={index} onSelect={handleSelect} className="rev_slider">
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    height="500"
-                    style={{objectFit:"cover"}}
-                    src="/images/slider/slide1.webp"
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>РАБОТАЕМ С ПЕРЕДОВЫМИ КОМПАНИЯМИ</h3>
-                </Carousel.Caption>
-            </Carousel.Item>
+
             <Carousel.Item>
                 <img
                     className="d-block w-100"
                     src="/images/slider/slide2.webp"
                     style={{objectFit:"cover"}}
-                    height="500"
+                    height="720"
                     alt="Second slide"
                 />
 
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <Carousel.Caption style={{top:'35%',bottom:'0'}}>
+                    <h3 style={{'font-size':'56pt', textAlign:'start'}}>РАБОТАЕМ С ПЕРЕДОВЫМИ КОМПАНИЯМИ</h3>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -41,13 +29,12 @@ function MainSlider() {
                     className="d-block w-100"
                     src="/images/slider/slide3.webp"
                     style={{objectFit:"cover"}}
-                    height="500"
+                    height="720"
                     alt="Second slide"
                 />
 
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <Carousel.Caption style={{top:'35%', bottom:'0'}}>
+                    <h3 style={{'font-size':'56pt', textAlign:'start'}}>РАБОТАЕМ С ПЕРЕДОВЫМИ КОМПАНИЯМИ</h3>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
