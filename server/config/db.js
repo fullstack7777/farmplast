7d13ef1784563a0441f3722fccd5a9d0f05a0b23
bega <akiyev9@gmail.com>
2022-10-02 15:58:05 +0500

    added placeholder

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 
 const db = mysql.createPool({
     host: "213.139.209.67",
-    user: "opencartr1",
+    user: "opencartr",
     password: "PDT3gHQ&",
     database:"opencart"
 })
