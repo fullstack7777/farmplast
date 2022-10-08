d9e3847221ab4fec2a1e281d9a7c2747c78e59fb
bega <akiyev9@gmail.com>
2022-10-08 17:45:30 +0500

    added getProductById

--- front/src/components/base/new_nav_bar.js
+++ front/src/components/base/new_nav_bar.js
@@ -38,12 +38,16 @@ function NewNavBar() {
     const handleChange = (e) => {
         setToSend({ ...toSend, [e.target.name]: e.target.value });
     };
+    let mapCategories = new Map();
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const fetchData = () => {
         return fetch("https://api.farmplst.com/api/getAllCategories")
             .then((response) => response.json())
-            .then((data) => setCategories(data));
+            .then(function (data) {
+                mapCategories.clear();
+                return setCategories(data);
+            });
     }
     useEffect(() => {
         fetchData();
@@ -68,9 +72,12 @@ function NewNavBar() {
                     >
                         <Nav.Link className="nav-bar-lg" href="/" >Главная</Nav.Link>
                         <NavDropdown className="nav-bar-lg" title="Продукция" id="navbarScrollingDropdown">
+                            {/* eslint-disable-next-line array-callback-return */}
                             {categories.map(function (category, index){
+                                if(category.parent_id!==0 && !mapCategories.has(category.name)){
+                                    mapCategories.set(category.name, 1);
                                     return <NavDropdown.Item href={'/products?category_ids='+category.category_id} >{category.name}</NavDropdown.Item>
-                                }
+                                }}
                             )}
                         </NavDropdown>
                        {/* //  Old Category and Subcategory*/}
