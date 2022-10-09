6f10823a74665aa1635a6e0861015401ee1942bc
bega <akiyev9@gmail.com>
2022-10-09 09:12:13 +0500

    added search api

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const db = require('./config/db')
 const cors = require('cors')
 const crypto = require("crypto");
-
 const app = express();
 
 const  PORT = 8080;
