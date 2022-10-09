eeb705c857c41458a755646eb951490a60e2ff78
bega <akiyev9@gmail.com>
2022-10-09 16:54:23 +0500

    added search api

--- front/src/components/products/product_modal.js
+++ front/src/components/products/product_modal.js
@@ -6,61 +6,12 @@ import Carousel from 'react-bootstrap/Carousel';
 
 
 function MyVerticallyCenteredModal(props) {
-    //const productId = props.id;
     const product = props.product1;
-    // const [jokes, setJokes] = useState();
-    // const [product, setProduct] = useState([]);
 
-    // const fetchData = () => {
-    //     fetch('https://api.farmplst.com/api/getProductById?id='+productId)
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //             setProduct(data);
-    //         })
-    //         .catch((err) => {
-    //             console.log(err.message);
-    //         });
-    // }
 
-    // useEffect(() => {
-    //     fetchData();
-    // }, [])
 
-    //
-    //
-    // const getJokes = () => {
-    //       fetch("https://api.farmplst.com/api/newSession")
-    //           .then((response) => response.json())
-    //           .then(result => console.log(result))
-    //           .catch(error => console.log('error', error));
-    // };
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("session", "0040d80a9cc8dd3b9a19630117");
-    urlencoded.append("quantity", "1");
-    urlencoded.append("product_id", "83");
-
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: urlencoded,
-        redirect: 'follow'
-    };
-
-    const addCart = () => {
-        fetch("https://api.farmplst.com/api/addCard", requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
-    }
     console.log(product);
     if(product!==undefined && product!=null && product!=undefined){
-   // let ext = getExtension(product.images);
-        //ext = product.image.replace('.'+ext,'-250x250.'+ext);
-        //let exts = getExtension(product.images);
         return (
             <Modal
                 {...props}
@@ -99,7 +50,7 @@ function MyVerticallyCenteredModal(props) {
                             <h4>{product.name}</h4>
                             <p>{product.model}</p>
                             <div className="button-cart-buy">
-                                <Button onClick={()=> addCart()} variant="primary custom-button" style={{width:'75%', borderRadius:'0px', marginBottom: 20,marginLeft: 20}}>Купить в один клик</Button>
+                                <Button variant="primary custom-button" style={{width:'75%', borderRadius:'0px', marginBottom: 20,marginLeft: 20}}>Купить в один клик</Button>
                                 <Button  variant="primary custom-button" style={{width:'75%', borderRadius:'0px', marginBottom: 20,marginLeft: 20}}>Заказать в один клик</Button>
                             </div>
                             <p style={{marginTop: 20, fontWeight: "bold"}}><span>Марка:</span> {product.tag}
@@ -107,7 +58,7 @@ function MyVerticallyCenteredModal(props) {
                             <p style={{marginTop: 20, fontWeight: "bold"}}>
                                 <span>Производитель:</span> {product.manufacturer}</p>
                             <div><span style={{fontWeight: "bold"}}>Описание:</span>
-                                <div style={{display:'grid'}}>{product.description!=null? product.description.replace(/[|&;$%@"<>()+,]/g,''):''}</div>
+                                <div style={{display:'grid', fontSize:'13px'}} dangerouslySetInnerHTML={{__html: htmlDecode(product.description)}}/>
                             </div>
                         </Col>
                     </Row>
@@ -119,5 +70,9 @@ function MyVerticallyCenteredModal(props) {
         );
     }
 }
-
+function htmlDecode(input){
+    let e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
 export default MyVerticallyCenteredModal;
