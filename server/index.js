c14b10a1992127ef4717bc89a78643112dda5311
bega <akiyev9@gmail.com>
2022-10-12 16:33:56 +0500

    added search api

--- server/index.js
+++ server/index.js
@@ -6,9 +6,17 @@ const nodemailer = require("nodemailer");
 const app = express();
 
 const  PORT = 8080;
-app.use(
-    cors({origin: ['https://farmplst.com', 'http://localhost:3000']})
-);
+// app.use(
+//     cors({origin: ['https://farmplst.com', 'http://localhost:3000']})
+// );
+
+const corsOptions ={
+    origin:'*',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
